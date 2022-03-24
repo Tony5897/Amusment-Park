589747fde02bf4aba3da88153357dc287379a19b
tonym <tony.martinez5897@gmail.com>
2022-03-24 10:05:31 -0700

    This will add script to highlight eligible rides.
    co-authored-by: Caleb Coughenour <CalebCoughenour@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 // Business Logic ----------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
- 
-  }
+
+  const height = parseInt(prompt("How tall are you?"));
+  const classHighlight = 'highlight';
+
+  if (height < 4) {
+    alert("Have Fun With The Kiddos!");
+    $('#kidRides').addClass(classHighlight);
+  } else {
+    alert("Have Fun & Be Safe!");
+    $('#adultRides').addClass(classHighlight);
+  } 
+
+  
 });
 
 
